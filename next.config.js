99afd48c8a46bc1aab1a8f62a0df5fcf6eb66161
Lino <sauvairelino@gmail.com>
2026-09-04 18:16:24 +0200

    feat: ajout gsap + lenis, alias webpack @/*, baseUrl tsconfig, dockerignore tsbuildinfo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   output: 'standalone',
+  webpack: (config) => {
+    // Alias explicite @/* → ./src/* (robustesse dans tous les environnements).
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
+  },
 };
 
 module.exports = nextConfig;
